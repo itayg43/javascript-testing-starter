b0fd42247b3d1ebd1f87b4de61e7640722a6d94c
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-06-03 08:02:22 +0300

    edit: complete mockes

---
+++ tests/mocking.test.js
@@ -0,0 +1,173 @@
+import { describe, expect, it, vi } from "vitest";
+import { trackPageView } from "../src/libs/analytics";
+import { getExchangeRate } from "../src/libs/currency";
+import { sendEmail } from "../src/libs/email";
+import { charge } from "../src/libs/payment";
+import security from "../src/libs/security";
+import { getShippingQuote } from "../src/libs/shipping";
+import {
+  getDiscount,
+  getPriceInCurrency,
+  getShippingInfo,
+  isOnline,
+  login,
+  renderPage,
+  signUp,
+  submitOrder,
+} from "../src/mocking";
+
+vi.mock("../src/libs/currency");
+vi.mock("../src/libs/shipping");
+vi.mock("../src/libs/analytics");
+vi.mock("../src/libs/payment");
+vi.mock("../src/libs/email", async (importOriginal) => {
+  const originalModule = await importOriginal();
+
+  return {
+    ...originalModule,
+    sendEmail: vi.fn(),
+  };
+});
+
+describe("getPriceInCurrency", () => {
+  it("should return price in target currency", () => {
+    vi.mocked(getExchangeRate).mockReturnValue(1.5);
+
+    const price = getPriceInCurrency(10, "AUD");
+
+    expect(price).toBe(15);
+  });
+});
+
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable if quote cannot be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+
+    expect(getShippingInfo("London")).toMatch(/unavailable/i);
+  });
+
+  it("should return shipping info quote can be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
+
+    expect(getShippingInfo("London")).toMatch(
+      /shipping cost: \$10 \(2 days\)/i
+    );
+  });
+});
+
+describe("renderPage", () => {
+  it("should return correct content", async () => {
+    const result = await renderPage();
+
+    expect(result).toMatch(/content/i);
+  });
+
+  it("should call analytics", async () => {
+    await renderPage();
+
+    expect(trackPageView).toHaveBeenCalledWith("/home");
+  });
+});
+
+describe("submitOrder", () => {
+  const creditCard = { creditCardNumber: "1234" };
+  const order = { totalAmount: 100 };
+
+  it("should charge the customer", async () => {
+    vi.mocked(charge).mockResolvedValue({ status: "success" });
+
+    await submitOrder(order, creditCard);
+
+    expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount);
+  });
+
+  it("should return success when payment is successful", async () => {
+    vi.mocked(charge).mockResolvedValue({ status: "success" });
+
+    const result = await submitOrder(order, creditCard);
+
+    expect(result).toEqual({ success: true });
+  });
+
+  it("should return error when payment is failed", async () => {
+    vi.mocked(charge).mockResolvedValue({ status: "failed" });
+
+    const result = await submitOrder(order, creditCard);
+
+    expect(result).toEqual({ success: false, error: "payment_error" });
+  });
+});
+
+describe("signUp", () => {
+  const email = "test@email.com";
+
+  it("should return false if email is not valid", async () => {
+    const result = await signUp("a");
+
+    expect(result).toBe(false);
+  });
+
+  it("should return true if email is valid", async () => {
+    const result = await signUp(email);
+
+    expect(result).toBe(true);
+  });
+
+  it("should send the welcome email if email is valid", async () => {
+    await signUp(email);
+
+    expect(sendEmail).toHaveBeenCalledOnce();
+    const args = vi.mocked(sendEmail).mock.calls[0];
+    expect(args[0]).toBe(email);
+    expect(args[1]).toMatch(/welcome/i);
+  });
+});
+
+describe("login", () => {
+  const email = "test@test.com";
+
+  it("should email the one-time login code", async () => {
+    const spy = vi.spyOn(security, "generateCode");
+
+    await login(email);
+
+    const securityCode = spy.mock.results[0].value.toString();
+    expect(sendEmail).toHaveBeenCalledWith(email, securityCode);
+  });
+});
+
+describe("isOnline", () => {
+  it("should return false if current hour is outside opening hours", () => {
+    vi.setSystemTime("2024-01-01 07:59");
+    expect(isOnline()).toBe(false);
+
+    vi.setSystemTime("2024-01-01 20:01");
+    expect(isOnline()).toBe(false);
+  });
+
+  it("should return true if current hour is within opening hours", () => {
+    vi.setSystemTime("2024-01-01 08:00");
+    expect(isOnline()).toBe(true);
+
+    vi.setSystemTime("2024-01-01 19:59");
+    expect(isOnline()).toBe(true);
+  });
+});
+
+describe("getDiscount", () => {
+  it("should return .2 on christmas day", () => {
+    vi.setSystemTime("2024-12-25 00:01");
+    expect(getDiscount()).toBe(0.2);
+
+    vi.setSystemTime("2024-12-25 23:59");
+    expect(getDiscount()).toBe(0.2);
+  });
+
+  it("should return 0 on any other day", () => {
+    vi.setSystemTime("2024-12-24 00:01");
+    expect(getDiscount()).toBe(0);
+
+    vi.setSystemTime("2024-12-26 00:01");
+    expect(getDiscount()).toBe(0);
+  });
+});
